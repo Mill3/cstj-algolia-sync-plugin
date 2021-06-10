95a85aca85fb379971372db2a984d4005d7f87ad
Antoine Girard <thetoine@gmail.com>
2021-06-10 17:32:23 -0400

    reverted text-cleanup active

--- algolia.php
+++ algolia.php
@@ -5,7 +5,7 @@
  * GitHub Plugin URI:  https://github.com/Mill3/cstj-algolia-sync-plugin
  * Plugin Name: CSTJ - Algolia Sync
  * Description: Sync data from Wordpress to Algolia
- * Version: 0.1.15
+ * Version: 0.1.16
  * Author Name: Mill3 Studio (Antoine Girard)
  *
  * @package CSTJ_Algolia_Sync
