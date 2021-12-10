5a23b1b4c333a8e5a24bcd7b2846db00fe090b2b
Antoine Girard <thetoine@gmail.com>
2021-12-10 10:54:09 -0500

    programs: added external_url ACF field

--- post_types/Programs.php
+++ post_types/Programs.php
@@ -15,7 +15,7 @@ class Programs extends WpAlgoliaRegisterAbstract implements WpAlgoliaRegisterInt
 {
     public $searchable_fields = array('post_title');
 
-    public $acf_fields = array('code', 'brochure', 'title', 'subtitle', 'body', 'why_title', 'why_subtitle', 'perspectives_title', 'perspectives_subtitle', 'verify_title');
+    public $acf_fields = array('code', 'brochure', 'title', 'subtitle', 'body', 'why_title', 'why_subtitle', 'perspectives_title', 'perspectives_subtitle', 'verify_title', 'external_url');
 
     public $taxonomies = array('duration', 'programs_type', 'profile', 'condition', 'period');
 
